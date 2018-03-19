bdb185c76b349ea53305276228ab08d311b27d46
Tim <timothystahl@gmail.com>
2018-03-19 09:02:32 -0600

    added routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    //use this to access object inside custom method
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
